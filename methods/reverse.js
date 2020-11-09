d89dab62e7b86a2f079d267d0ea6a128c1a703fc
aouos <aouos@qq.com>
2020-11-09 11:19:38 +0800

    Array reverse

---
+++ methods/reverse.js
@@ -0,0 +1,23 @@
+let arr = [2, 3, 4, 6];
+// arr.reverse();
+
+// The first way
+Object.prototype.reverseArr = function () {
+  let newArr = [];
+  for (let i = this.length - 1; i >= 0; i--) {
+    newArr.push(this[i]);
+  }
+  return newArr;
+}
+
+// The second way
+Object.prototype.reverseArr = function () {
+  for (let i = 0, j = this.length - 1; i < j; i++, j--) {
+    let temp = this[i];
+    this[i] = this[j];
+    this[j] = temp;
+  }
+  return this;
+}
+
+console.log(arr.reverseArr());
